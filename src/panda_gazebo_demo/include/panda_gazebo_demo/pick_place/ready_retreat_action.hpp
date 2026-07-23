28a6d6b08640ef6463ef7d612a092694271090bd
zjumty <zjumty@aliyun.com>
2026-07-23 14:33:34 +0800

    feat: add ready retreat action

---
+++ src/panda_gazebo_demo/include/panda_gazebo_demo/pick_place/ready_retreat_action.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "panda_gazebo_demo/pick_place/gripper_command_adapter.hpp"
+#include "panda_gazebo_demo/pick_place/moveit_motion_adapter.hpp"
+#include "panda_gazebo_demo/pick_place/named_target_validation.hpp"
+#include "panda_gazebo_demo/pick_place/state_action.hpp"
+
+namespace panda_gazebo_demo::pick_place
+{
+
+struct ReadyRetreatConfig
+{
+  State state;
+  State next_state;
+  std::string named_target;
+  std::map<std::string, double> ready_joint_positions;
+  double joint_tolerance;
+  double close_position;
+  double max_effort;
+};
+
+class ReadyRetreatAction final : public IStatePlanner, public IStateExecutor
+{
+public:
+  ReadyRetreatAction(
+    std::shared_ptr<IMoveItMotionAdapter> motion,
+    std::shared_ptr<IGripperCommandAdapter> gripper,
+    std::shared_ptr<IWorldObserver> observer, ReadyRetreatConfig config);
+
+  [[nodiscard]] PlanResult plan(
+    State current_state, State next_state,
+    const ObservationResult & observation) override;
+  [[nodiscard]] ActionResult execute(const ExecutionContext & context) override;
+  [[nodiscard]] ActionResult cancel() override;
+
+private:
+  std::shared_ptr<IMoveItMotionAdapter> motion_;
+  std::shared_ptr<IGripperCommandAdapter> gripper_;
+  std::shared_ptr<IWorldObserver> observer_;
+  ReadyRetreatConfig config_;
+};
+
+}  // namespace panda_gazebo_demo::pick_place
