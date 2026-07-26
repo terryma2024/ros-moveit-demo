491b55f815db1427fea06ad3671a4ef28b203fd4
zjumty <zjumty@aliyun.com>
2026-07-27 03:53:35 +0800

    feat: add fact-driven SO-101 recovery policy

---
+++ src/so101_gazebo_demo/include/so101_gazebo_demo/pick_place/so101_recovery_policy.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "so101_gazebo_demo/pick_place/recovery_policy.hpp"
+#include "so101_gazebo_demo/pick_place/so101_profile.hpp"
+
+namespace so101_gazebo_demo::pick_place
+{
+
+class SO101RecoveryPolicy final : public IRecoveryPolicy
+{
+public:
+  explicit SO101RecoveryPolicy(SO101Profile profile = SO101Profile::canonical());
+
+  [[nodiscard]] RecoveryRoute select(State failed_state, const Failure & original_failure,
+                                     const WorldSnapshot & current) const override;
+
+private:
+  SO101Profile profile_;
+};
+
+}  // namespace so101_gazebo_demo::pick_place
